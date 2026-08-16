8c9ee6eec49809e74448b013a4a0cb0b1b73b3ed
Connor <99201695+connorxjt@users.noreply.github.com>
2026-08-15 20:20:26 -0400

    Fix namespace declaration

--- Sources/ConnorUI/ConnorUI.swift
+++ Sources/ConnorUI/ConnorUI.swift
@@ -3,6 +3,5 @@ public enum ConnorUI {
     /// The development version represented by the current foundations branch.
     public static let version = "0.2.0-development"
 
-    private init() {}
 }
 
