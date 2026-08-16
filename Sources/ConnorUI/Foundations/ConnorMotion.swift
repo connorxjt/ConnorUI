57e1d2e30b382ae57207dcb99034342136fd43e5
Connor <99201695+connorxjt@users.noreply.github.com>
2026-08-15 20:20:41 -0400

    Fix motion namespace declaration

--- Sources/ConnorUI/Foundations/ConnorMotion.swift
+++ Sources/ConnorUI/Foundations/ConnorMotion.swift
@@ -6,6 +6,5 @@ public enum ConnorMotion {
     public static let standard = Animation.easeInOut(duration: 0.24)
     public static let tactile = Animation.spring(response: 0.28, dampingFraction: 0.78)
 
-    private init() {}
 }
 
